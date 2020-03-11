8f83314b07763bfac0c1f865ecaed7de4c4f6d13
Micah Cowan <micah@addictivecode.org>
2020-03-11 02:25:42 -0700

    Fixed erroneous patch offset.

--- patch.asm
+++ patch.asm
@@ -2,4 +2,6 @@
 ;; can be played with two players on a Famicom by using
 ;; the player two "A" button in place of (the nonexistant) "Start"
 
-*= $1E894	and #$90
+; Original ROM location is $1E894.
+; We need to add 16 ($10) to that for the iNES header.
+*= $1E8A4	and #$90
